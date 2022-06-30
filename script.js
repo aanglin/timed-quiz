010f5e2dc000320d6ee479014157711452e24435
Aaron Anglin <aaron.anglin101@gmail.com>
2022-06-29 19:45:36 -0500

    initial Commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// Psuedo Code
+// 1. Create landing pagewith start button to start game.
+// 2. When game started, the timer is started and the first question is presented.
+//  3. When the question is answered, then if it is right you continue
+//  4. If the question is wrong, time is taken off the timer and you continue.
+//  5. When all questions are answered or the timer runs out,game over
+//  6. When game over, my score is shown.
+//  7. I can then save my initials and my score
+
+// var questions = {
+
+// {
+//     title: "Commonly used data types DO NOT include:",
+//     choices: ["strings", "booleans", "alerts", "numbers"],
+//     answer: "alerts"
+//   },
+//   {
+//     title: "The condition in an if / else statement is enclosed within .",
+//     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+//     answer: "parentheses"
+//   },
+//   {
+//     title: "Arrays in JavaScript can be used to store .",
+//     choices: [
+//       "numbers and strings",
+//       "other arrays",
+//       "booleans",
+//       "all of the above"
+//     ],
+//     answer: "all of the above"
+//   },
+//   {
+//     title:
+//       "String values must be enclosed within __ when being assigned to variables.",
+//     choices: ["commas", "curly brackets", "quotes", "parentheses"],
+//     answer: "quotes"
+//   },
+//   {
+//     title:
+//       "A very useful tool used during development and debugging for printing content to the debugger is:",
+//     choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+//     answer: "console.log"
+//   }
+// ];
